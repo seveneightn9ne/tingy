1ab1aab18ccdb0307130273aabf19257e7037c26
Miles Steele <miles@milessteele.com>
2016-12-18 11:04:40 -0500

    always run

--- bundle.js
+++ bundle.js
@@ -55,30 +55,24 @@
 	        return {
 	            currentPuzzle: tingy.puzzles[0],
 	            code: tingy.puzzles[0].starterCode,
-	            result: "",
 	            submissions: {},
 	            currentSubmission: 0
 	        };
 	    },
 	    render: function () {
-	        console.log(this.state);
+	        var result = tingy.evaluateIt(this.state.code);
 	        return (React.createElement("div", null,
 	            React.createElement("p", null, this.state.currentPuzzle.description),
-	            React.createElement("p", null, "Ask a guard a question by passing them a function that takes the correct path" + " " + "(either \"left\" or \"right\") and returns a boolean. The guard will evaluate your" + " " + "query, taking the correct path into consideration, and return you an answer depending" + " " + "on the guard's own disposition."),
+	            React.createElement("p", null, "Ask a guard a question by passing them a function that takes the correct path" + " " + "(either \"left\" or \"right\") and returns a boolean. The guard will evaluate your" + " " + "query, taking the correct path into consideration, and return you an answer depending" + " " + "on the guard\\'s own disposition."),
 	            React.createElement("textarea", { value: this.state.code, onChange: this.changeCode, cols: 80, rows: 6 }),
 	            React.createElement("br", null),
 	            React.createElement("br", null),
-	            React.createElement("button", { onClick: this.tryIt }, "Try it"),
+	            React.createElement("button", null, "Try it"),
 	            React.createElement("button", { onClick: this.reset }, "Reset"),
-	            React.createElement("p", null, this.state.result)));
+	            React.createElement("p", null, result)));
 	    },
 	    changeCode: function (event) {
-	        console.log("hi");
-	        console.log(event.target.value);
 	        this.setState({ code: event.target.value });
-	    },
-	    tryIt: function () {
-	        this.setState({ result: tingy.evaluateIt(this.state.code) });
 	    },
 	    reset: function () {
 	        this.setState({ code: this.state.currentPuzzle.starterCode, result: "" });
--- test.tsx
+++ test.tsx
@@ -9,32 +9,26 @@ const Tingy = React.createClass({
   getInitialState: function() {return {
     currentPuzzle: tingy.puzzles[0],
     code: tingy.puzzles[0].starterCode,
-    result: "",
     submissions: {},
     currentSubmission: 0,
   }},
   render: function () {
-    console.log(this.state);
+    let result = tingy.evaluateIt(this.state.code);
     return (<div>
       <p>{this.state.currentPuzzle.description}</p>
       <p>Ask a guard a question by passing them a function that takes the correct path
       (either "left" or "right") and returns a boolean. The guard will evaluate your
       query, taking the correct path into consideration, and return you an answer depending
-      on the guard's own disposition.</p>
+      on the guard\'s own disposition.</p>
       <textarea value={this.state.code} onChange={this.changeCode}  cols={80} rows={6} />
       <br /><br />
-      <button onClick={this.tryIt}>Try it</button>
+      <button>Try it</button>
       <button onClick={this.reset}>Reset</button>
-      <p>{this.state.result}</p>
+      <p>{result}</p>
     </div>);
   },
   changeCode: function(event) {
-    console.log("hi");
-    console.log((event.target as any).value);
     this.setState({code: (event.target as any).value});
-  },
-  tryIt: function() {
-    this.setState({result: tingy.evaluateIt(this.state.code)});
   },
   reset: function() {
     this.setState({code: this.state.currentPuzzle.starterCode, result: ""});
